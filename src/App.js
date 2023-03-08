4da95a4146dffccec5cabcce3f930f64ed43996b
Jay <116917734+jaycode16@users.noreply.github.com>
2023-03-08 01:53:07 -0600

    Removing older chatgpt api key from public repro

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
-// "Explain things like you would to a 10 year old learning how to code."
+const API_KEY = "ADD API KEY HERE OR USE DOTENV MODULE";
+// IE: "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Act as an azure app service expert. You will take a querie and provide me with information and output links to assit me"
 }
